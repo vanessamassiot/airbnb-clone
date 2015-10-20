a0425aa57b6c64fd0711f6c564f665d7b6cb0d1c
baptiste mulliez <baptistem59@hotmail.com>
2015-10-20 15:05:52 +0200

    creation of bookings from client

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,32 @@
 class BookingsController < ApplicationController
-  def new
+  before_action :find_flat, only: [:new, :create]
 
+  def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new (booking_params)
+    @booking[:client_id] = current_user.id
+    @booking[:flat_id] = @flat.id
+    @booking[:status] = "envoyée"
+    if @booking.valid?
+      @booking.save
+      redirect_to account_booking_path(@booking)
+    else
+      raise
+      render :new
+    end
 
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:checkin, :checkout)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:flat_id])
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ActiveRecord::Base
   belongs_to :flat
   belongs_to :client, class_name: "User", foreign_key: "client_id"
 
-  validates_presence_of :checkin, :checkout, :status
+  validates_presence_of :checkin, :checkout
   validates :status, inclusion: { in: ["envoyée", "confirmée", "refusée"], allow_nil: false }
 
 end
