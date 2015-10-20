3764806d2f270ef5f05a49e9e60fa958792557be
Francois xavier Abraham <fix@doum.it>
2015-10-20 12:17:21 +0200

    create flats_controller

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,8 @@
+class FlatsController < ApplicationController
+  def index
+
+  end
+
+  def show
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 User.create(name: "Riri", email: "riri@lewagon.org", password: "12345678", password_confirmation: "12345678")
-User.create(name: "Fifi", email: "fifi@lewagon.org", password: "12345679", , password_confirmation: "12345679")
-User.create(name: "Loulou", email: "loulou@lewagon.org", password: "12345671", , password_confirmation: "12345671")
-User.create(name: "Tom", email: "tom@lewagon.org", password: "12345672", , password_confirmation: "12345672")
-User.create(name: "Jerry", email: "jerry@lewagon.org", password: "12345673", , password_confirmation: "12345673")
+User.create(name: "Fifi", email: "fifi@lewagon.org", password: "12345679", password_confirmation: "12345679")
+User.create(name: "Loulou", email: "loulou@lewagon.org", password: "12345671", password_confirmation: "12345671")
+User.create(name: "Tom", email: "tom@lewagon.org", password: "12345672", password_confirmation: "12345672")
+User.create(name: "Jerry", email: "jerry@lewagon.org", password: "12345673", password_confirmation: "12345673")
 
-user.last.flats.create(title: "Pool House", description: "Maison au bord de la plage avec Piscine", beds: 3, street: "12 rue de la plage", city: "La Baule",
+User.last.flats.create(title: "Pool House", description: "Maison au bord de la plage avec Piscine", beds: 3, street: "12 rue de la plage", city: "La Baule",
 postcode: "44500", price: 89)
-user.last.flats.create(title: "Maison de famille", description: "Maison en forêt idéale famille nombreuse", beds: 15, street: "22 allée des Pins", city: "Mayenne",
+User.last.flats.create(title: "Maison de famille", description: "Maison en forêt idéale famille nombreuse", beds: 15, street: "22 allée des Pins", city: "Mayenne",
 postcode: "53000", price: 125)
-user.first.flats.create(title: "Studio", description: "Idéal pour week end à deux pour profiter de la vie parisienne", beds: 2, street: "48 avenue verdier", city: "Montrouge",
+User.first.flats.create(title: "Studio", description: "Idéal pour week end à deux pour profiter de la vie parisienne", beds: 2, street: "48 avenue verdier", city: "Montrouge",
 postcode: "92000", price: 69)
-user.first.flats.create(title: "Matelas dans le coin du salon", description: "matelas gonflable. pas très confortable mais pratique", beds: 1, street: "2 rue de la Soif", city: "Rennes",
+User.first.flats.create(title: "Matelas dans le coin du salon", description: "matelas gonflable. pas très confortable mais pratique", beds: 1, street: "2 rue de la Soif", city: "Rennes",
 postcode: "35000", price: 14)
-user.first.flats.create(title: "Appartement Terrasse", description: "vue sur la rout eiffel, plein centre, idéalement situé", beds: 5, street: "5 avenue Georges V", city: "Paris",
+User.first.flats.create(title: "Appartement Terrasse", description: "vue sur la rout eiffel, plein centre, idéalement situé", beds: 5, street: "5 avenue Georges V", city: "Paris",
 postcode: "75008", price: 276)
 
 
-client.last.bookings.create(flat: Flat.last, checkin: Date.today, checkout: Date.current + 2.days, status: "envoyée")
+User.last.bookings.create(flat: Flat.first, checkin: Date.today, checkout: Date.current + 2.days, status: "envoyée")
