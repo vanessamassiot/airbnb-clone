0be20ffc332abc04da51ddf4f57485925e69c347
vanessa massiot <vanessa.massiot@free.fr>
2015-10-20 16:45:06 +0200

    bookings show and index

--- app/controllers/account/bookings_controller.rb
+++ app/controllers/account/bookings_controller.rb
@@ -1,9 +1,11 @@
 module Account
   class BookingsController < ApplicationController
     def index
+      @bookings = current_user.bookings
     end
 
     def show
+      @booking = Booking.find(params[:id])
     end
   end
 end
