23e1957acb292f1960f5a1483657180c45cf8f26
Flix Abraham <fix@doum.it>
2015-10-20 15:20:56 +0200

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ActiveRecord::Base
   belongs_to :flat
   belongs_to :client, class_name: "User", foreign_key: "client_id"
 
-  validates_presence_of :checkin, :checkout
+  validates_presence_of :checkin, :checkout, :status, :client_id, :flat_id
   validates :status, inclusion: { in: ["envoyée", "confirmée", "refusée"], allow_nil: false }
 
 end
