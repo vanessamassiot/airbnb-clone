f13d8d66c082a98d6c0c58238c32fe62f111f9e5
baptiste mulliez <baptistem59@hotmail.com>
2015-10-21 11:02:21 +0200

    get rid of turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
