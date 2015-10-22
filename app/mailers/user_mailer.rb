8cada27b079d7fa497258e05d938226c1019bf8b
vanessa massiot <vanessa.massiot@free.fr>
2015-10-22 11:06:42 +0200

    test mail ok

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
   def welcome(user)
     @user = user
 
-    mail (to: @user.email, subject "Welcome to the Best AirBnB"
+    mail(to: @user.email, subject: "Welcome to the Best AirBnB")
   end
 end
