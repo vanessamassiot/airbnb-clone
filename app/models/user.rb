a24a339acfea854fb31eb3ed01a02a8e57e7f894
vanessa massiot <vanessa.massiot@free.fr>
2015-10-22 15:43:20 +0200

    confirmable dans user works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, omniauth_providers: [:facebook], :confirmable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+  devise :omniauthable, omniauth_providers: [:facebook]
 
   has_many :flats,    foreign_key: "owner_id", dependent: :destroy
   has_many :bookings, foreign_key: "client_id", dependent: :destroy
