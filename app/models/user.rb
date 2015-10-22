e80a877affb2799d0a3b0cfec1ea35c4fd06188f
vanessa massiot <vanessa.massiot@free.fr>
2015-10-22 16:28:38 +0200

    mail transactionnel welcome

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
   has_many :flats,    foreign_key: "owner_id", dependent: :destroy
--- db/migrate/20151022133320_add_confirmation_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddConfirmationToUsers < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-    end
-  end
-end
