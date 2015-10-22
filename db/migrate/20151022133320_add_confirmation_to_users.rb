bc6b32c0bd8bf9e3544e703de9dc80a7e6afb484
vanessa massiot <vanessa.massiot@free.fr>
2015-10-22 15:37:09 +0200

    migration ok addconfirmation to users

---
+++ db/migrate/20151022133320_add_confirmation_to_users.rb
@@ -0,0 +1,9 @@
+class AddConfirmationToUsers < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+    end
+  end
+end
