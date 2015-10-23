03d4de8cc8a977a7ff6ecd32b2f03cf970bca4fb
baptiste mulliez <baptistem59@hotmail.com>
2015-10-23 16:44:30 +0200

    booking validations and view

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ActiveRecord::Base
 
   validates_presence_of :checkin, :checkout, :status, :client_id, :flat_id
   validates :status, inclusion: { in: ["send", "confirmed", "refused"], allow_nil: false }
+  validates_inclusion_of :checkin, in: ->(booking) { (Time.now .. booking.checkout) },
+                         message: 'Hey Marty Mc Fly !! IRL checkout must be after checkin !'
 end
