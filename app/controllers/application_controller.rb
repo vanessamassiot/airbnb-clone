eca2486fd3ceb0d7616162347496e1daad454b9c
baptiste mulliez <baptistem59@hotmail.com>
2015-10-23 12:04:57 +0200

    voilou

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  helper_method :current_user
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
 
   after_filter :store_location
 
