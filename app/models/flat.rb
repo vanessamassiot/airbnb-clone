45819c700d993ca9cdf97530bb1a7e381b03f672
Francois xavier Abraham <fix@doum.it>
2015-10-23 15:54:16 +0200

    Fix AWS SDK gem version RTFM oLOL

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,12 +1,12 @@
 class Flat < ActiveRecord::Base
-  geocoded_by :full_address
-  after_validation :geocode, if: :full_address_changed?
-
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   has_many :bookings, dependent: :destroy
 
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
+
+  geocoded_by :full_address
+  after_validation :geocode, if: :full_address_changed?
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
